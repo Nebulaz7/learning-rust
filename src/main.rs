4f41a9b03be9661423e8b3fa914667a7b4b6e903
Nebulaz7 <jp0712002@gmail.com>
2026-03-22 02:16:53 +0100

    learning rust

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+   // my first rust code
+    println!("main\nmy name is joshua");
+    let name = "my ex";
+    println!("I am in love with {}", name);
+
+    let mut x = 5;
+    
+    println!("x = {}", x);
+    x = 10;
+
+    println!("x = {}", x);
+}
