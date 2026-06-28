f7e77ece94e8356a685f66c68a8c0b46abe9ca03
Nebulaz7 <jp0712002@gmail.com>
2026-06-28 18:17:56 +0100

    refactor: remove unused code and simplify main function; add recall function

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,4 @@
 fn main() {
-    // my first rust code
-    println!("main\nmy name is ");
-    let mut x = 5;
-
-    println!("x = {}", x);
-    x = 10;
-    let mut ectasy = "stuff";
-
-    let y = x + 5;
-
-    const MY_CONSTANT: i32 = 87365;
-
-    println!("My constant value is {}", MY_CONSTANT);
-
-    println!("x = {}", y);
-
-    let is_rust_good = true;
-
-    println!("Is Rust good? {}", is_rust_good);
-
-    let age = 30;
-    let can_vote = age >= 18;
-
-    println!("Can I vote? {}", can_vote);
-
-    let is_logged_in = false;
-
-    if is_logged_in {
-        println!("Welcome back!")
-    } else {
-        println!("Please Log in..............")
-    }
-
     let month = 3;
     //   let more_months = 12; syff
 
@@ -43,4 +10,16 @@ fn main() {
         5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 => println!("Months coming soon!!"),
         _ => println!("Invalid date"),
     };
+
+    let mut count = 1;
+    loop {
+        println!("The value is {}", count);
+
+        if count == 15 {
+            println!("The loop is completed at value {}", count);
+            break;
+        }
+
+        count += 1;
+    }
 }
